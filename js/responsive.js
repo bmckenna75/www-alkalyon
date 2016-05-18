75a4403f88ddd0624e33bf6a2ef9ac970ba0d198
Chris Guillott <chris@techfo.xyz>
2016-05-18 10:02:15 -0400

    responsive changes

--- js/responsive.js
+++ js/responsive.js
@@ -1,6 +1,5 @@
-if ($(window).width() > 700) {
-  $(document).ready(function() { $(".arrow").hide();});
-  $(window).scroll(function() {
+$(window).scroll(function() {
+  if ($(window).width() > 700) {
     var scroll = $(window).scrollTop();
     var nav = $(".nav-container");
     var diff = scroll - nav.offset().top;
@@ -9,14 +8,36 @@ if ($(window).width() > 700) {
     } else {
       $(".sticky").removeClass("sticky");
     }
-  });} else {
-    $(document).ready(function() {
-      $(".nav").hide();
-      $(".dropdown-content").hide();
-      $(".nav-head").click(function(){$(".nav").toggle();$(".nav-head .arrow").toggleClass("rotate90");});
-      $(".dropdown>a").click(function(){
+  }
+});
+
+$(document).ready(function() {
+  $(".nav-head").click(function(){$(".nav").toggle();$(".nav-head .arrow").toggleClass("rotate90");});
+  $(".dropdown>a").click(function(){
+    if ($(window).width() < 700) {
       $(".dropdown-content").toggle();
       $(".dropdown .arrow").toggleClass("rotate90");
-    });
+    }
   });
-}
+  if ($(window).width() >= 700) {
+    $(".arrow").hide();
+    $(".nav").css("display", "flex");
+    $(".dropdown-content").css("display", "block");  
+  } else {
+    $(".nav").hide();
+    $(".dropdown-content").hide();
+    
+  }
+});
+
+$(window).resize(function() {
+   if ($(window).width() >= 700) {
+    $(".arrow").hide();
+    $(".nav").show();
+    $(".dropdown-content").css("display", "block");  
+  } else {
+    $(".nav").hide();
+    $(".arrow").show();
+    $(".dropdown-content").hide();
+  }
+});
